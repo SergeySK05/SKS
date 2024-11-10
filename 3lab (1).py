11022c327a304b3b987adc7e5a863956ad88312f
Gleb_SS <152023307+Infinity-sound-OK@users.noreply.github.com>
2024-11-11 02:50:04 +0700

    Update 3lab (1).py

--- 3lab (1).py
+++ 3lab (1).py
@@ -12,6 +12,18 @@ def showdata(): #txt в таблицу
         r.append(s)
     return r
 def find_num(): #нахождение последнего номера
+    d = open('data_source.txt',encoding='utf-16').readlines()
+    r=[]
+    number=0
+    for i in d:
+        s = [x for x in i.split("/")]
+        r.append(s)
+    for i in range(len(r)):
+        num1=r[i][0]
+        if int(num1)>number:
+            number=int(num1)
+    return number
+def find_num(): #нахождение последнего номера
     d = open('D:\python\pythonProject\data_source.txt',encoding='utf-16').readlines()
     r=[]
     number=0
